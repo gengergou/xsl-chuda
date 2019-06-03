b98dfe45a52ebe0d67422041e2c1261c48542c8a
耿二狗 <geng12306@163.com>
2019-06-03 16:05:44 +0800

    第三次提交

---
+++ src/Test2.java
@@ -0,0 +1,15 @@
+/**
+ * @Title: Test2
+ * @Package: PACKAGE_NAME
+ * @Description:
+ * @Auther: rui
+ * @Date: 2019/6/3
+ * @version: V1.0
+ */
+public class Test2 {
+	public static void main(String[] args) {
+
+		System.out.println("你好");
+	}
+
+}
