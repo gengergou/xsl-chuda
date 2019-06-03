51090f70f7487485cd913791c2a52cbb8f1d9c56
耿二狗 <geng12306@163.com>
2019-06-03 16:01:02 +0800

    23

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @Title: Test
+ * @Package: PACKAGE_NAME
+ * @Description:
+ * @Auther: rui
+ * @Date: 2019/6/3
+ * @version: V1.0
+ */
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("测试");
+	}
+}
